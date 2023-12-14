e757358d67eba14d0a03e5729bb3a982d8199cde
SEA-BEAST <153852624+SEA-BEAST@users.noreply.github.com>
2023-12-14 12:24:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask import render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def home():
+    return render_template("index.html")
+
+@app.route("/<name>/")
+def error(name):
+    return f"<h1>error</h1> <h4>this page has gone extinct. go back to the main website.</h4>"
+
+if __name__ = "__main__":
+    app.run(host="0.0.0.0", port=5000)
